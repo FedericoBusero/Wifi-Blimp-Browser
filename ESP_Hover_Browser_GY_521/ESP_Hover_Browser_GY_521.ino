c4b80aeb21400cfdd198b134d3e4e64210f124ce
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2023-08-10 12:04:32 +0200

    Update ESP_Hover_Browser_GY_521.ino

--- ESP_Hover_Browser_GY_521/ESP_Hover_Browser_GY_521.ino
+++ ESP_Hover_Browser_GY_521/ESP_Hover_Browser_GY_521.ino
@@ -238,7 +238,7 @@ void updateMotors()
     */
      
       int z_motorsnelheid = map(ui_slider2,0,360,0,PWM_RANGE); // voor zweefmotor
-      if (abs((ui_joystick_y * ui_joystick_x) < 5) {
+      if (abs(ui_joystick_y * ui_joystick_x) < 5) {
         z_motorsnelheid = 0; // bij joystick los ook zweefmotor uit
       }
 
